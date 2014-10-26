993011ef3a56e9a62a4d6fb329aeba9a275ebf32
macg19-91 <mcastro19.12.91@gmail.com>
2014-10-25 21:15:35 -0600

    Get Partidos

--- app/scripts/controllers/partidos.js
+++ app/scripts/controllers/partidos.js
@@ -11,10 +11,12 @@ angular.module('cuantoQuedaApp')
 
   .controller('PartidosCtrl', function ($scope,ServiceHandler) {
         $scope.leagueMatches= [];// End "sales" Array.
+
+        var listPartidos=[];
             
         //[{id:0,id_Equipo1:1,id_Equipo2:2,goles_Equipo1:0,goles_Equipo2:0,fecha:"2014-10-18 20:00:00",id_Liga:1}];
         //alert($scope.leagueMatches[0].matches[0].fecha);
-        console.log($scope.leagueMatches);
+        //console.log($scope.leagueMatches);
         var today = new Date();
         var dd = today.getDate();
         var mm = today.getMonth()+1; //January is 0!
@@ -41,11 +43,11 @@ angular.module('cuantoQuedaApp')
         //alert(today);
 
         ServiceHandler.get('serviceLiga'
-        ).then(function(response){
-            if(response){
+        ).then(function( responseLiga){
+            if( responseLiga){
                 console.log('correcto');              
-                for(var i = 0; i < response.data.length; i++){
-                  getPartido(today,response.data[i].id,response.data[i].Nombre);
+                for(var i = 0; i <  responseLiga.data.length; i++){
+                  getPartido("2014-10-14 00:00:00", responseLiga.data[i].id, responseLiga.data[i].Nombre);
                 }
             }
             else{
@@ -54,14 +56,11 @@ angular.module('cuantoQuedaApp')
         });
 
         function getPartido(date,idL,liga){
-            console.log('idLiga'+idL);
-            ServiceHandler.get('servicePartido',{fechaPartido:date,idLiga:idL}
-                    ).then(function(response){
-                        if(response){
-                            var parsed = (response);                
-                            for(var j in parsed){
-                             $scope.leagueMatches.push({name:liga,matches:j});
-                            }
+            ServiceHandler.get('servicePartido'//,{fechaPartido:date,idLiga:idL}
+                    ).then(function(  responsePartido){
+                        if(  responsePartido){
+                             $scope.leagueMatches.push({name:liga,matches:responsePartido.data});
+                           
                             console.log($scope.leagueMatches);
                         }
                         else{
@@ -72,10 +71,10 @@ angular.module('cuantoQuedaApp')
   });
 
         /*ServiceHandler.get('servicePartido',{idPartido:1}
-        ).then(function(response){
-            if(response){
+        ).then(function( responseLiga){
+            if( responseLiga){
                 //console.log('correcto');
-                var parsed = JSON.parse(response);                
+                var parsed = JSON.parse( responseLiga);                
                 for(var i in parsed){
                   parsed[i].id_Equipo1=getEquipo(parsed[i].id_Equipo1);
                   parsed[i].id_Equipo2=getEquipo(parsed[i].id_Equipo1);
